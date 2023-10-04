9d1ba39c16cad64f4f4e67bc1a46c2e62222d4e2
youjin kim <gracecancho@gmail.com>
2023-10-04 16:03:54 +0900

    made an error

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-import { init, exit } from "./myPackage";
+// import * as crypto from "crypto"; // 이렇게 쓰기 싫으면 tsconfig 수정 "esModuleInterop" : true
+import crypto from "crypto";
 
-init();
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    // initialize hash
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+  }
+}
