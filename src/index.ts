7a6f8c6c734d4203d13d688c360e49b5194af1c5
youjin kim <gracecancho@gmail.com>
2023-10-05 13:15:06 +0900

    finished

--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,39 @@ class Block implements BlockShape {
     return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    // Block (prevHash, height, data)
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    return [...this.blocks]; // new block에 얕은 복사 추가했더니 밑에 해킹코드가 안먹음
+  }
+}
+
+const blockchain = new Blockchain();
+
+// 누구나 사용
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+
+// Hacking
+blockchain.getBlocks().push(new Block("xxxxx", 111111, "HACKEDDDD!"));
+
+console.log(blockchain.getBlocks());
